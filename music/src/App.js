d9c8428369503c569da1319563273b0a8bdfc541
unknown <edisonalexanderarias@gmail.com>
2022-06-21 12:49:44 -0700

    Displayed the data (song title, album, artist, genre, and release date) from the API within a table on the frontend.

--- music/src/App.js
+++ music/src/App.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
   useEffect(() => {
    makeGetRequest();
   }, [])
@@ -17,12 +20,35 @@ function App() {
     }
   }
 
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <button></button>
-    </div>
-  );
-}
+async function createSong(newSong){
+//  New Song:
+//  {
+//      "title": "Dreamer",
+//      "artist": "I The Mighty",
+//      "album": "Karma Never Sleeps",
+//      "genre": "Alternative/Indie, Rock",
+//      "release_date": "2012-03-27
+//  }
+  let response = await axios.post('http://127.0.0.1:8000/api/songs/', newSong);
+  if(response.status === 201){
+    await getAllSongs();
+  }
+}  
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+            <th>song title</th>
+            <th>album</th>
+            <th>artist</th>
+            <th>genre</th>
+            <th>release_date</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
