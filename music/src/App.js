69d508d4a4d17306109fde18f199839a45dca438
unknown <edisonalexanderarias@gmail.com>
2022-06-21 12:26:10 -0700

    Used AXIOS to consume the Django REST API on the frontend.

--- music/src/App.js
+++ music/src/App.js
@@ -1,8 +1,26 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  useEffect(() => {
+   makeGetRequest();
+  }, [])
+
+  async function makeGetRequest(){
+    try{
+      let response = await axios.get('http://127.0.0.1:8000/api/songs/');
+
+      console.log(response.data);
+    } catch (ex) {
+      console.log('Error in MakeGetRequest API call!');
+    }
+  }
+
   return (
     <div>
- 
+      <h1>Hello World</h1>
+      <button></button>
     </div>
   );
 }
