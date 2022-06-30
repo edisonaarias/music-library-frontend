40fed723a9fb723b3bbd20643f3000bce34a722f
unknown <edisonalexanderarias@gmail.com>
2022-06-30 14:44:44 -0700

    Moified the search bar to be able to search for a song by 5 diffferent terms

--- music/src/Components/SearchBar.jsx
+++ music/src/Components/SearchBar.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 
-
 //  New Song:
 //  {
 //      "title": "The Outside",
@@ -9,33 +8,62 @@ import { useEffect } from 'react';
 //      "genre": "Metal/Christian rock",
 //      "release_date": "2011-02-01"
 //  }
+// Filter by album, artist, genre, release date, and title
 
-const SearchBar = (props) => {
 
-    return ( 
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Artist</th>
-                    <th>Album</th>
-                    <th>Genre</th>
-                    <th>Release Data</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {props.parentSongs.map((song, index) => {
-                        return (
-                            <tr>
-                            <td>{index + 1}</td>
-                            <td>{song.title}</td>
-                            <td>{song.artist}</td>
-                            </tr>
-                        );
-                })}        
-                </tbody>
-            </table>
+/**Function made to find a song with 5 (terms album, artist, genre, release date, and title)
+ * 
+ * @param {Array} Triatsperson  A collection of songs-objects 
+ * @param {return} people       A collection of songs-objects 
+ */
+
+ function searchForFiveTerms(Termssong, songs) {
+    let results = song.filter(
+         }
+        x = '5 terms';
+        y = new RegExp(/a/g);
+        while(null != (z=y.exec(x))) {
+            console.log(z);    
+            console.log(z[0]);
+    }
+
+            const str = 'a a a';
+            const re = /a/g;
+
+        while(match = re.exec(str)){
+            console.log(match, ' found at : ', match.index); 
+
+        
     );
-}
+    console.log(results);
+    return results;
 
-export default SearchBar;
+// const SearchBar = (props) => {
+
+//     return ( 
+//             <table>
+//                 <thead>
+//                 <tr>
+//                     <th>Title</th>
+//                     <th>Artist</th>
+//                     <th>Album</th>
+//                     <th>Genre</th>
+//                     <th>Release Data</th>
+//                 </tr>
+//                 </thead>
+//                 <tbody>
+//                     {props.parentSongs.map((song, index) => {
+//                         return (
+//                             <tr>
+//                             <td>{index + 1}</td>
+//                             <td>{song.title}</td>
+//                             <td>{song.artist}</td>
+//                             </tr>
+//                         );
+//                 })}        
+//                 </tbody>
+//             </table>
+//     );
+// }
+
+// export default SearchBar;
